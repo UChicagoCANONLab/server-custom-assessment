86fbdd8a802f6cf51031a14f8aee1bcf686fdb39
Connor <chopcraft@uchicago.edu>
2018-08-10 11:43:04 -0500

    test

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const app = express()
-const Scratch = require('scratch-api')
-const port = 3000;
+const express = require('express');
+const app = express();
+//const Scratch = require('scratch-api');
+const port = process.env.PORT || 3000;
 
-var id = 192728047
+var id = 192728047;
 
 app.get('/', function(req, res) {
+	/*
 	Scratch.getProject(id,function(err,project) {
-  		if(err) throw err
-  		res.send(JSON.stringify(project))
-  	})
-})
+  		if(err) throw err;
+  		res.send(JSON.stringify(project));
+  	});
+  	*/
+  	res.send("hey");
+});
 
-app.listen(port, function() {console.log(`Example app listening on port http://localhost:${port}/`)})
+app.listen(port, function() {console.log(`Example app listening on port http://localhost:${port}/`)});
