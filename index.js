b184c08da97921cd0aadb77f53ae78f3456f5ec6
Connor <chopcraft@uchicago.edu>
2018-08-10 13:37:35 -0500

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//const Scratch = require('./scratchapi.js');
+const Scratch = require('./scratchapi.js');
 const port = process.env.PORT || 3000;
 var id = 192728047;
 
