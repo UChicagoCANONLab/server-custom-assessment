c70660865cf12c4c2ae1c333d29c62d12573f630
Connor <chopcraft@uchicago.edu>
2018-08-10 12:04:50 -0500

    asdfhuajsLKDFa

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
-const Scratch = require('./scratchapi.js');
+//const Scratch = require('./scratchapi.js');
 const port = 3000;
 
 var id = 192728047;
 
 app.get('/', function(req, res) {
-	
+	/*
 	Scratch.getProject(id,function(err,project) {
   		if(err) throw err;
   		res.send(JSON.stringify(project));
   	});
+  	/*
+  	res.send("hi");
  });
 
 app.listen(port, function() {console.log(`Example app listening on port http://localhost:${port}/`)});
