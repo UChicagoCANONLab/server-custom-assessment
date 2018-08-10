f37a5d3e0859b4169cd6c5f036bc6a39a6d7e7c2
Connor <chopcraft@uchicago.edu>
2018-08-10 13:38:37 -0500

    another one

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const port = process.env.PORT || 3000;
 var id = 192728047;
 
 app.get('/', function(req, res) {
-	/*
+	
 	Scratch.getProject(id,function(err,project) {
   		if(err) throw err;
-  		res.send(JSON.stringify(project));
+  		//res.send(JSON.stringify(project));
+  		res.send("hi");
+
   	});
-  	*/
-  	res.send("hi");
+  	
  });
 
 app.listen(port, function() {console.log(`Example app listening on port http://localhost:${port}/`)});
