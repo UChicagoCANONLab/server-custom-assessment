d347838bc1d5f41ab9149de4f534ad38be4f3bbb
Connor <chopcraft@uchicago.edu>
2018-08-16 16:51:31 -0500

    what?

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-var cors = require('cors')
+var cors = require('cors');
 const app = express();
 const Scratch = require('./scratchapi.js');
 const port = process.env.PORT || 3000;
 var id = 192728047;
 
-
-app.use(cors())
+app.use(cors());
 
 app.get('/:id', function(req, res) {
 	Scratch.getProject(req.params.id,function(err,project) {
