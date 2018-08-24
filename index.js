63d08cf27096e9a0d0bc960bea71edb561bf602b
Connor <chopcraft@uchicago.edu>
2018-08-24 16:10:38 -0500

    readme + readability

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const Scratch = require('./scratchapi.js');
 const port = process.env.PORT || 3000;
 var id = 192728047;
 
+/* Prevents same-origin policy errors. */
 app.use(cors());
 
+/* Recives url/[project id]. */
 app.get('/:id', function(req, res, next) {
     var id = req.params.id;
 	    Scratch.getProject(id,function(err,project) {
 	      if(err) {
-	      	//console.log(err);
 	      	res.status(404).send("404 - cannot find.")
-	      	//res.send("Fetch error.");
 	      }
 	      else {
+	   		/* Return project JSON if everything goes well. */
 	      	res.send(JSON.stringify(project));
 	      }
     	});
  });
 
-
+/* For local testing. */
 app.listen(port, function() {console.log(`Example app listening on port http://localhost:${port}/`)});
