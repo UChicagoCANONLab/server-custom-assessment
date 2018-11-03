4840fa2438f6c949b17368abeb42bbf42b1ac8a7
jeansalac <salac@canon.uchicago.edu>
2018-11-03 15:51:08 -0500

    Updated bash script to take in template file path for copying

--- run_unit2gen.sh
+++ run_unit2gen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#Run with ./run_unit2gen.sh timestamp
+#Run with ./run_unit2gen.sh [dynamic folder path] [template folder path]
+
+cp -a "$2/." $1
 
 #Go into timestamp folder
 cd $1
@@ -8,10 +10,8 @@ cd $1
 #Change permissions on Question Generator
 chmod +x unit2QuestionGenerator.py
 
-infoFile="$1.csv"
-
 #Run question generator
-python unit2QuestionGenerator.py $infoFile
+python unit2QuestionGenerator.py students.csv
 
 #Read csv of students
 IFS=","
@@ -21,7 +21,11 @@ do
         rm "$f3".json
         rm "$f3"_jsonString.txt
         
-        #plug in picture generator here
+        #plug in picture generator here; returns svg fils
+
+
+        #convert svgs to pngs
+        #for file in *.svg; do inkscape $file -e ${file%svg}png; done
         
         #mkdir "$f3"_images
         #mv *.png "$f3"_images
@@ -29,7 +33,7 @@ do
         #Generate pdfs
         pdflatex "$f3"_test.tex
 
-done < $infoFile
+done < students.csv
 
 #Get rid of extra files
 rm *.aux
